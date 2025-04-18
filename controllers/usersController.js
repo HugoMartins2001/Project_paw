b89ad425f4c7f361e0919392031962dc9c4844f9
Hugo Martins <hugolm280@hotmail.com>
2025-04-18 22:40:52 +0100

    update in show users

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,9 +3,12 @@ const mongoUser = require("../models/user");
 let usersController = {};
 
 usersController.showUsers = async (req, res, next) => {
+
+  const user = req.user;
+
   try {
     const users = await mongoUser.find({}, { password: 0 });
-    res.render("users/showUsers", { users });
+    res.render("users/showUsers", { users, user });
   } catch (err) {
     next(err);
   }
