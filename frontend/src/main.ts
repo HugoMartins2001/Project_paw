f38ed887bf6736096e12f287cc5526e4188a2b45
Hugo Martins <hugolm280@hotmail.com>
2025-05-12 14:41:56 +0100

    .

--- frontend/src/main.ts
+++
@@ -1,5 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { appConfig } from './app/app.config'; // usa a config correta
-
-bootstrapApplication(AppComponent, appConfig); // 👈 esta linha é suficiente
