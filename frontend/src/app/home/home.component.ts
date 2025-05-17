03d837e029e364f7c04d2156f46d3a4e31f7f683
Hugo Martins <hugolm280@hotmail.com>
2025-05-17 20:10:59 +0100

    .

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -7,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
+
+  navigateToMenus() {this.router.navigate(['/menus']);}
+  navigateToRestaurants() {this.router.navigate(['/restaurants']);}
+  navigateToRegister() {this.router.navigate(['/register']);}
+  navigateToDishes() {this.router.navigate(['/dishes']);}
   
 }
