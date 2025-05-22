31efcccf550c1eaf9de3d0262dc63e05d7a64a0e
Hugo Martins <hugolm280@hotmail.com>
2025-05-22 14:34:55 +0100

    updates

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -13,13 +13,56 @@ export class FooterComponent {
 
   constructor(private router: Router) {}
 
-  navigateToHome(): void { this.router.navigate(['/home']); }
-  navigateToMenu(): void { this.router.navigate(['/menus']); }
-  navigateToLogin(): void { this.router.navigate(['/login']); }
-  navigateToRestaurant(): void { this.router.navigate(['/restaurants']); }
+  navigateToHome(): void {
+    const role = localStorage.getItem('role');
+    if (!role) {
+      this.router.navigate(['/home/user']);
+    } else if (role === 'Client') {
+      this.router.navigate(['/client/home']);
+    } else if (role === 'Admin' || role === 'Manager') {
+      this.router.navigate(['/home']);
+    } else {
+      this.router.navigate(['/home/user']);
+    }
+  }
+  navigateToMenu(): void {
+    const role = localStorage.getItem('role');
+    if (!role) {
+      this.router.navigate(['/menus/user']);
+    } else if (role === 'Client') {
+      this.router.navigate(['/client/menus']);
+    } else if (role === 'Admin' || role === 'Manager') {
+      this.router.navigate(['/menus']);
+    } else {
+      this.router.navigate(['/menus/user']);
+    }
+  }
+  navigateToRestaurant(): void {
+    const role = localStorage.getItem('role');
+    if (!role) {
+      this.router.navigate(['/restaurants/user']);
+    } else if (role === 'Client') {
+      this.router.navigate(['/client/restaurant']);
+    } else if (role === 'Admin' || role === 'Manager') {
+      this.router.navigate(['/restaurants']);
+    } else {
+      this.router.navigate(['/restaurants/user']);
+    }
+  }
   navigateToAbout(): void { this.router.navigate(['/about']); }
   navigateToContact(): void { this.router.navigate(['/contact']); }
   navigateToRegister(): void { this.router.navigate(['/register']); }
-
+  navigateToDishes(): void {
+    const role = localStorage.getItem('role');
+    if (!role) {
+      this.router.navigate(['/dishes/user']);
+    } else if (role === 'Client') {
+      this.router.navigate(['/client/dishes']);
+    } else if (role === 'Admin' || role === 'Manager') {
+      this.router.navigate(['/dishes']);
+    } else {
+      this.router.navigate(['/dishes/user']);
+    }
+  }
 
 }
